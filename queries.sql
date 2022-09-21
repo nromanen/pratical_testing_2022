24aa4daa6db153b0950f770d0d63dfec9579b6f5
Nataliia Romanenko <nromanen@softserveinc.com>
2022-09-21 19:41:40 +0300

    add sql task

---
+++ queries.sql
@@ -0,0 +1,34 @@
+Liubomyr
+1.	Show the list of french customers’ names who have made more than one order (use grouping).
+2.	Show the list of french customers’ names who have made more than one order (use a subquery).
+3.	Show the list of customers’ names who used to order the ‘Tofu’ product (use a subquery).
+
+Liza
+4.	*Show the list of customers’ names who used to order the ‘Tofu’ product, along with the total amount of the product they have ordered and with the total sum for ordered product calculated.
+
+Dasha
+5.	*Show the list of french customers’ names who used to order non-french products (use left join).
+
+Nataliia V
+6.	*Show the list of french customers’ names who used to order non-french products (use a subquery).
+
+Misha
+7.	*Show the list of french customers’ names who used to order french products.
+
+Vika
+8.	*Show the total ordering sum calculated for each country of customer.
+
+Ira
+9.	*Show the total ordering sums calculated for each customer’s country for domestic and non-domestic products separately (e.g.: “France – French products ordered – Non-french products ordered” and so on for each country).
+
+Vita
+10.	*Show the list of product categories along with total ordering sums calculated for the orders made for the products of each category, during the year 1997.
+
+Nik
+11.	*Show the list of employees’ names along with names of their chiefs (use left join with the same table).
+12.	*Show the list of cities where employees and customers are from and where orders have been made to. Duplicates should be eliminated.
+
+Natalii F
+13.	Show first, last names and ages of 3 eldest employees.
+14.	Show the list of all cities where the employees are from.
+
